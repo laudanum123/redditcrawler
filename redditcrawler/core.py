62eba16efce0f984c4bd12c13b912275043ad2ab
laudanum <>
2022-11-05 01:06:19 +0100

    change to be more robust

--- redditcrawler/core.py
+++ redditcrawler/core.py
@@ -1,16 +1,19 @@
+import datetime as dt
+import sqlite3
 from platform import python_branch
 from traceback import print_list
-import praw
-import pandas as pd
-import datetime as dt
-import nltk
-from tld import get_fld
-import sqlite3
-from tqdm import tqdm
-import helpers
-import matplotlib.pyplot as plt
-tqdm.pandas()
+from typing import cast
 
+import matplotlib.pyplot as plt
+import pandas as pd
+import praw
+from tld import get_fld
+from tqdm import tqdm
+from psaw import PushshiftAPI
+
+import helpers
+
+tqdm.pandas()
 
 def augment_submission_data(subreddit_name):
     '''Adds augmented columns for all submissions not yet processed in the database'''
@@ -19,10 +22,11 @@ def augment_submission_data(subreddit_name):
                          client_secret='5xx3LIrF_a9s6CLU2K8q46dmzv6l6w',
                          user_agent='USER_AGENT',
                         )
+    api = PushshiftAPI(reddit)
 
     reddit.subreddit(subreddit_name)
 
-    con = sqlite3.connect('reddit.db')
+    con = sqlite3.connect('data/db/reddit.db')
     cur = con.cursor()
     try:
         cur.execute('SELECT * FROM submissions WHERE subreddit=? AND domain_name IS NULL AND url IS NOT NULL', (subreddit_name,))
@@ -56,32 +60,56 @@ def augment_submission_data(subreddit_name):
 
 def plot_author_distribution(subreddit_name):
     '''Plots the distribution of authors in the database'''
-    con = sqlite3.connect('./reddit.db')
+    con = sqlite3.connect('data/db/reddit.db')
     cur = con.cursor()
     cur.execute('SELECT author_name, COUNT(*) FROM submissions WHERE subreddit=? GROUP BY author_name ORDER BY COUNT(*) DESC LIMIT 100', (subreddit_name,))
     rows = cur.fetchall()
     con.close()
     df = pd.DataFrame(rows, columns=['author', 'count'])
     df['percentage'] = df['count'] / df['count'].sum()
-    df.plot.bar(x='author', y='percentage')
-    plt.show()
+    df.plot.bar(x='author', y='percentage', title='{} Author Distribution'.format(subreddit_name), figsize=(20,15))
+    plt.savefig('./output/{}_author_distribution.png'.format(subreddit_name))
+
 
 def plot_domain_distribution(subreddit_name):
     '''Plots the distribution of domains in the database'''
-    con = sqlite3.connect('./reddit.db')
+    con = sqlite3.connect('data/db/reddit.db')
     cur = con.cursor()
     cur.execute('SELECT domain_name, COUNT(*) FROM submissions WHERE subreddit=? GROUP BY domain_name ORDER BY COUNT(*) DESC LIMIT 100', (subreddit_name,))
     rows = cur.fetchall()
     con.close()
     df = pd.DataFrame(rows, columns=['domain', 'count'])
     df['percentage'] = df['count'] / df['count'].sum()
-    df.plot.bar(x='domain', y='percentage')
-    plt.show()
+    df.plot.bar(x='domain', y='percentage', title='{} Author Distribution'.format(subreddit_name), figsize=(20,15))
+    plt.savefig('./output/{}_domain_distribution.png'.format(subreddit_name))
+
+def plot_time_distribution(subreddit_name: str):
+    '''Plots the distribution of submissions over time in the database'''
+    con = sqlite3.connect('data/db/reddit.db')
+    cur = con.cursor()
+    cur.execute('SELECT created FROM submissions WHERE subreddit=?', (subreddit_name,))
+    rows = cur.fetchall()
+    con.close()
+    df = pd.DataFrame(rows, columns=['created'])
+    df['created'] = pd.to_datetime(df['created'])
+    df = df.groupby(pd.Grouper(key='created', freq='D'))['created'].count()
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    df.plot.line(x='created', y='count', title='{} Time Distribution'.format(subreddit_name), style='.-', legend=False, figsize=(20,10))
+    
+    for i,j in df.items():
+        ax.annotate(str(j),xy=(i,j))  # type: ignore
+        if i.weekday() == 5:  # type: ignore
+            ax.axvspan(i, i + dt.timedelta(days=1), alpha=0.2, color='red')  # type: ignore
+
+    plt.savefig('./output/{}_time_distribution.png'.format(subreddit_name))
 
 if __name__ == '__main__':
-    subreddit_name = 'UkraineConflict'
-    helpers.grab_new_submissions(subreddit_name, limit=None)
+    subreddit_name = 'AskReddit'
+    helpers.grab_new_submissions(subreddit_name, only_new=False, limit=500)
     augment_submission_data(subreddit_name)
     plot_author_distribution(subreddit_name)
     plot_domain_distribution(subreddit_name)
+    plot_time_distribution(subreddit_name)
     
--- redditcrawler/helpers.py
+++ redditcrawler/helpers.py
@@ -1,58 +1,75 @@
+import datetime as dt
 import sqlite3
+
 import pandas as pd
 import praw
-import datetime as dt
+from psaw import PushshiftAPI
 from tqdm import tqdm
+
 
 def convert_datatypes(df):
     '''Converts the datatypes of the columns in the dataframe'''
-    convert_dict = {'id': str, 'title': str, 'subreddit':str, 'score':int, 'url': str, 'comms_num':int, 'body': str}
+    convert_dict = {'id': str, 'title': str, 'subreddit': str,
+                    'score': int, 'url': str, 'comms_num': int, 'body': str}
     df_submission = df.astype(convert_dict)
     return df_submission
+
 
 def turn_pkl_to_sql(df):
     '''Turns the reddit.pkl file into a sqlite3 database'''
     df_submission = convert_datatypes(df)
 
-    with sqlite3.connect('reddit.db') as con:
+    with sqlite3.connect('data/db/reddit.db') as con:
         df_submission.to_sql('submissions', con, if_exists='replace')
         con.commit()
         con.close()
 
-def get_date(created):
+
+def get_date(created : int):
     return dt.datetime.fromtimestamp(created)
 
-def grab_new_submissions(subreddit_name, limit=None):
+
+def grab_new_submissions(subreddit_name: str, only_new: bool = True, limit: int = 0):
     '''Retrieve new submissions for subreddit and add them to the DB'''
 
     # Get the last submission we have in the database
-    con = sqlite3.connect('reddit.db')
-    cur = con.cursor()
-    try:
-        cur.execute('SELECT MAX(created) FROM submissions WHERE subreddit=?', (subreddit_name,))
-        last_submission = cur.fetchone()[0]
-        last_submission = dt.datetime.strptime(last_submission, '%Y-%m-%d %H:%M:%S')
-    except:
+    if only_new:
+        con = sqlite3.connect('data/db/reddit.db')
+        cur = con.cursor()
+        try:
+            cur.execute(
+                'SELECT MAX(created) FROM submissions WHERE subreddit=?', (subreddit_name,))
+            last_submission = cur.fetchone()[0]
+            last_submission = dt.datetime.strptime(
+                last_submission, '%Y-%m-%d %H:%M:%S')
+        except sqlite3.DatabaseError as e:
+            print('Error: ', e)
+            last_submission = dt.datetime(1970, 1, 1, 0, 0, 0)
+        con.close()
+    else:
         last_submission = dt.datetime(1970, 1, 1, 0, 0, 0)
-    con.close()
 
     # Get the new submissions from Reddit
     reddit = praw.Reddit(client_id='-NuD7EbzEKxtmdzCycYLCQ',
                          client_secret='5xx3LIrF_a9s6CLU2K8q46dmzv6l6w',
                          user_agent='USER_AGENT',
-                        )
-    subreddit = reddit.subreddit(subreddit_name)
+                         )
+    
+    api = PushshiftAPI(reddit)
+    
+    submissions = api.search_submissions(after=last_submission, subreddit=subreddit_name, limit=limit)
+                                         
+    dict = {"title": [],
+            "subreddit": [],
+            "score": [],
+            "id": [],
+            "url": [],
+            "comms_num": [],
+            "created": [],
+            "body": []}
 
-    dict =        { "title":[],
-                "subreddit":[],
-                "score":[], 
-                "id":[], 
-                "url":[], 
-                "comms_num": [], 
-                "created": [], 
-                "body":[]}
-
-    for submission in tqdm(subreddit.new(limit=limit)):
+    for i, submission_id in enumerate(tqdm(submissions)):
+        submission = reddit.submission(submission_id)
         if submission.stickied:
             continue
         else:
@@ -63,23 +80,20 @@ def grab_new_submissions(subreddit_name, limit=None):
             dict["url"].append(submission.url)
             dict["comms_num"].append(submission.num_comments)
             dict["created"].append(submission.created)
-            dict["body"].append(submission.selftext)    
-    
-    new_submissions_df = pd.DataFrame(dict)
-    new_submissions_df = convert_datatypes(new_submissions_df)
-    new_submissions_df["created"] = new_submissions_df['created'].apply(get_date)
-    
-    # Get all new submissions that are newer than the last submission we have in the database
-    if limit != 0:
-        new_submissions_df = new_submissions_df[new_submissions_df['created'] > last_submission]
-    
-    # Add the new submissions to the database
-    con = sqlite3.connect('reddit.db')
-    try:
-        lines_added = new_submissions_df.to_sql('submissions', con, if_exists='append', index=False)
-        print('{} lines added to the database'.format(lines_added))
-    except ValueError:
-        print('No new submissions')
-    con.commit()
-    con.close()
+            dict["body"].append(submission.selftext)
+            print(submission.title)
+        if (i+1)%20 == 0:
+            new_submissions_df = pd.DataFrame(dict)
+            new_submissions_df = convert_datatypes(new_submissions_df)
+            new_submissions_df["created"] = new_submissions_df['created'].apply(get_date)
 
+            # Add the new submissions to the database
+            con = sqlite3.connect('data/db/reddit.db')
+            try:
+                lines_added = new_submissions_df.to_sql('submissions', con, if_exists='append', index=False)
+                print('{} lines added to the database'.format(lines_added)) 
+                con.commit()
+            except ValueError:
+                print('No new submissions')
+            con.close()
+            new_submissions_df = pd.DataFrame()
